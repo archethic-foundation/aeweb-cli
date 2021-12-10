a718b741c479fa513ad6e9f96188ae178d18d664
manujuniris <90322956+manujuniris@users.noreply.github.com>
2021-12-10 19:32:04 +0530

    Initialize repository and CLI (#19)

---
+++ README.md
@@ -0,0 +1,51 @@
+# ÆWeb
+
+Welcome to the ÆWeb Cli repository ! This command line interface enables you to deploy files on top of ARCHEthic Public Blockchain.
+
+Using this cli you can deploy a single page website
+
+
+## Version
+
+1.0.0
+
+
+## Instructions
+
+To get the ÆWeb CLI, you need NodeJS installed. Then you need to install the CLI, using:
+```bash
+npm install aeweb -g
+```
+
+To generate address you need -
+- `seed` is a string representing the transaction chain entropy to be able to derive and generate the keys for the transactions
+- `index` is the number of transactions in the chain, to generate the current and the next public key 
+```bash
+aeweb generate-address --seed=myseedphrase --index=0
+```
+
+To deploy files you need -
+- `seed` is a string representing the transaction chain entropy to be able to derive and generate the keys
+- `index` is the number of transactions in the chain, to generate the current and the next public key 
+- `endpoint` is the URL of a welcome node to receive the transaction
+- `path` is the file path
+```bash
+aeweb deploy-file --seed=myseedphrase --index=0 --endpoint=https://testnet.archethic.net --path=./index.html
+```
+
+## Contribution
+
+Thank you for considering to help out with the source code. 
+We welcome contributions from anyone and are grateful for even the smallest of improvement.
+
+Please to follow this workflow:
+1. Fork it!
+2. Create your feature branch (git checkout -b my-new-feature)
+3. Commit your changes (git commit -am 'Add some feature')
+4. Push to the branch (git push origin my-new-feature)
+5. Create new Pull Request
+
+
+## Licence
+
+AGPL
---
+++ aeweb.js
@@ -0,0 +1,129 @@
+#!/usr/bin/env node
+const yargs = require('yargs')
+const archethic = require('archethic')
+const fs = require('fs')
+const chalk = require('chalk')
+const figlet = require('figlet')
+const mime = require('mime')
+
+
+
+yargs.command({
+    command: 'about',
+    describe: 'Welcome',
+   
+
+    handler: function (argv)
+    {   
+        console.log(chalk.green('\n','Hello and Welcome to AeWeb !','\n'))
+        console.log(chalk.blue(figlet.textSync('AeWeb',{font : "Alligator2"})))
+        console.log(chalk.green('\n','Create your Website on top of ArchEthic'))
+        console.log(chalk.green('\n','Version - 1.0.0','\n'))
+        
+    }
+})
+
+yargs.command({
+    
+    command: 'generate-address',
+    describe: 'Generate Address',
+    builder: {
+        seed: {
+            describe: 'Seed',
+            demandOption: true,  // Required
+            type: 'string'     
+        },
+        index: {
+            describe: 'Index',
+            demandOption: true,  // Required
+            type: 'number'     
+        }
+       
+    },
+    
+    handler: function (argv)
+    {   
+        const address = archethic.deriveAddress(argv.seed, argv.index)
+        console.log(chalk.blue(address))
+        console.log(chalk.green("If you are using testnet go to https://testnet.archethic.net/faucet & add some funds to the generated address, otherwise transfer funds from your UCO wallet (in Mainnet)"))
+    
+        
+    }
+})
+
+
+yargs.command({
+    command: 'deploy-file',
+    describe: 'Generate transaction on-chain',
+    builder: {
+        seed: {
+            describe: 'Seed',
+            demandOption: true,  // Required
+            type: 'string'     
+        },
+
+        index: {
+            describe: 'Index',
+            demandOption: true,  // Required
+            type: 'number'     
+        },
+
+        endpoint: {
+            describe: 'Node Endpoint',
+            demandOption: true,  // Required
+            type: 'string'     
+        },
+
+        path: {
+            describe: 'Path',
+            demandOption: true,  // Required
+            type: 'string'     
+        }
+
+
+    },
+
+    handler: function (argv)
+    {   function toHex(bytes) {
+                return bytes.reduce((str, byte) => str + byte.toString(16).padStart(2, '0'), '');
+        }
+        
+        const originPrivateKey = "01009280BDB84B8F8AEDBA205FE3552689964A5626EE2C60AA10E3BF22A91A036009"
+        let transaction
+
+        
+        const content = fs.readFileSync(argv.path)
+        
+        
+        
+      
+            transaction = null
+            txBuilder = archethic.newTransactionBuilder("hosting")
+                        .setContent(content)
+        
+        
+            
+        transaction = txBuilder
+                        .build(argv.seed, argv.index, argv.curve)
+                        .originSign(originPrivateKey)
+            
+        console.log (chalk.green(toHex(transaction.address)))
+        
+       
+        
+       
+        archethic.sendTransaction(transaction, argv.endpoint).then(() => {
+            
+            
+            
+            console.log(chalk.blue("Transaction Sent Successfully !"))
+            
+            console.log(chalk.green(argv.endpoint+"/api/last_transaction/"+(toHex(transaction.address))+"/content?mime="+mime.getType(argv.path)))
+            
+        })
+    }
+    })
+   
+
+    
+yargs.parse()
