8b033bd2f272a167f28e4f0b9120944309beb39d
Neylix <julien.leclerc05@protonmail.com>
2022-05-30 17:37:01 +0200

    Rework to handle complex website (#61)
    * Update dependency and set type to module
    * Deploying website according to new API where deployments are managed through a  JSON index instead of multiple chains
    * Print different message for file or folder deployment
    * Display explorer transaction link

--- commands/generate_address.js
+++ commands/generate_address.js
@@ -1,25 +1,32 @@
-const archethic = require('archethic')
-const chalk = require('chalk')
+import archethic from 'archethic'
+import chalk from 'chalk'
 
-exports.command = 'generate-address'
+const command = 'generate-address'
 
-exports.describe = 'Generate Address - to transfer some funds to this address'
+const describe = 'Generate Address - to transfer some funds to this address'
 
-exports.builder = {
-    seed: {
-        describe: 'Seed is a string representing the transaction chain entropy to be able to derive and generate the keys for the transactions',
-        demandOption: true, // Required
-        type: 'string'
-    },
-    index: {
-        describe: 'Index is the number of transactions in the chain, to generate the current and the next public key',
-        demandOption: true, // Required
-        type: 'number'
-    }
+const builder = {
+  seed: {
+    describe: 'Seed is a string representing the transaction chain entropy to be able to derive and generate the keys for the transactions',
+    demandOption: true, // Required
+    type: 'string'
+  },
+  index: {
+    describe: 'Index is the number of transactions in the chain, to generate the current and the next public key',
+    demandOption: true, // Required
+    type: 'number'
+  }
 }
 
-exports.handler = function (argv) {
-    const address = archethic.deriveAddress(argv.seed, argv.index)
-    console.log(chalk.blue(address))
-    console.log(chalk.green("If you are using testnet go to https://testnet.archethic.net/faucet & add some funds to the generated address, otherwise transfer funds from your UCO wallet (in Mainnet)"))
+const handler = function (argv) {
+  const address = archethic.deriveAddress(argv.seed, argv.index)
+  console.log(chalk.blue(address))
+  console.log(chalk.green("If you are using testnet go to https://testnet.archethic.net/faucet & add some funds to the generated address, otherwise transfer funds from your UCO wallet (in Mainnet)"))
+}
+
+export default {
+  command,
+  describe,
+  builder,
+  handler
 }
