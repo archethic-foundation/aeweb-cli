982e0db6944c379cc0242eedf7274d343df86fb7
Neylix <julien.leclerc05@protonmail.com>
2022-10-27 16:59:52 +0200

    Update deploy command with new API

---
+++ cli.js
@@ -0,0 +1,82 @@
+import path from 'path'
+import fs from 'fs'
+
+export function getSeeds(baseSeed) {
+  return {
+    refSeed: baseSeed + 'aeweb_ref',
+    filesSeed: baseSeed + 'aeweb_files'
+  }
+}
+
+export function loadSSL(sslCertificateFile, sslKeyFile) {
+  const sslConfiguration = {}
+
+  if (sslCertificateFile !== undefined) {
+    sslConfiguration.cert = fs.readFileSync(sslCertificateFile, 'utf8')
+  }
+
+  if (sslKeyFile !== undefined) {
+    sslConfiguration.key = fs.readFileSync(sslKeyFile, 'utf8')
+  }
+
+  return sslConfiguration
+}
+
+export function normalizeFolderPath(folderPath) {
+  return path.normalize(folderPath.endsWith(path.sep) ? folderPath.slice(0, -1) : folderPath)
+}
+
+export function getFiles(folderPath) {
+  let files = []
+  if (fs.statSync(folderPath).isDirectory()) {
+    handleDirectory(folderPath, files)
+
+    files = files.map(file => {
+      file.path = file.path.replace(folderPath, '')
+      return file
+    })
+  } else {
+    const data = fs.readFileSync(folderPath)
+    const path = path.basename(folderPath)
+    files.push({ path, data })
+  }
+
+  return files
+}
+
+export async function estimateTxsFees(archethic, transactions) {
+  const slippage = 1.01
+
+  let transactionsFees = transactions.map(tx => {
+    return new Promise(async (resolve, reject) => {
+      const { fee } = await archethic.transaction.getTransactionFee(tx)
+      resolve(fee)
+    })
+  })
+
+  transactionsFees = await Promise.all(transactionsFees)
+
+  // Last transaction of the list is the reference transaction
+  const fee = transactionsFees.pop()
+  const refTxFees = Math.trunc(fee * slippage)
+
+  let filesTxFees = transactionsFees.reduce((total, fee) => total += fee, 0)
+  filesTxFees = Math.trunc(filesTxFees * slippage)
+
+  return { refTxFees, filesTxFees }
+}
+
+function handleDirectory(entry, files) {
+  if (fs.statSync(entry).isDirectory()) {
+    fs.readdirSync(entry).forEach(child => {
+      handleDirectory(entry + path.sep + child, files)
+    });
+  } else {
+    handleFile(entry, files)
+  }
+}
+
+function handleFile(path, files) {
+  const data = fs.readFileSync(path)
+  files.push({ path, data })
+}
